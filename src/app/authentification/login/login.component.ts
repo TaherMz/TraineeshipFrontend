12987e8309c5d0ee8c64a589800b93d77b00e380
rahma19 <56127492+rahma19@users.noreply.github.com>
2020-11-25 22:02:09 +0100

    .

--- src/app/authentification/login/login.component.ts
+++ src/app/authentification/login/login.component.ts
@@ -1,15 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { DataService } from 'src/app/uniteStage/data.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [MessageService]
+
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  email?:any="";
+  password?:any="";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  }
+  constructor(private messageService:MessageService,private dataService:DataService,private router:Router,private http:HttpClient) { }
 
   ngOnInit(): void {
   }
-
+  Submit(form) {
+    
+    console.log ("form.value", form.value)
+    // if (form.valid) {
+         let addedData = JSON.stringify(form.value);
+         console.log ("addedData", addedData);
+        this.http.post(environment.api+"auth/login", addedData,this.httpOptions).subscribe((res) => {
+           this.messageService.add({severity:'success', summary: 'Message', detail:'Succes'});  
+           this.router.navigate(['/accueil']);
+         },
+           error => {
+             this.messageService.add({severity:'error', summary: ' Message', detail:'Erreur'});
+           })
+       ;
+     //}
+   }
 }
